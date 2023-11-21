33314a7b7d6adda666e26dfebe4fb18e503ef358
Str1nder <rm552238@fiap.com.br>
2023-11-20 22:27:59 -0300

    Ajustando e estilizando página contato

--- src/app/contato/page.jsx
+++ src/app/contato/page.jsx
@@ -14,7 +14,7 @@ export default function Contato(){
             <div className='about_me'>
                 <div className="about_me_textbox">
                     <h1 className='about_me_title'>Emile de Moura Maia</h1>
-                    <p className='about_me_subtitle'>FrontEnd e Designer</p>
+                    <p className='about_me_subtitle'>FrontEnd e Banco de dados</p>
                     <p className='about_me_rm'>RM: 552235 / 1TDSS</p>
                     <FaGithub className="icon_about_me"/>
                     <FaInstagram className="icon_about_me"/>
@@ -23,9 +23,67 @@ export default function Contato(){
                 <div className="about_me_image">
                     <Image src={'/Contato/emi.svg'} width={200} height={200}>
                     </Image>
-                    
                 </div>
-                
+            </div>
+
+            <div className='about_me'>
+                <div className="about_me_textbox">
+                    <h1 className='about_me_title'>Maria Eduarda Costa</h1>
+                    <p className='about_me_subtitle'>FrontEnd e Gestão de projetos</p>
+                    <p className='about_me_rm'>RM: 98760 / 1TDSS</p>
+                    <FaGithub className="icon_about_me"/>
+                    <FaInstagram className="icon_about_me"/>
+                    <FaLinkedinIn className="icon_about_me"/>
+                </div>
+                <div className="about_me_image">
+                    <Image src={'/Contato/maria.svg'} width={200} height={200}>
+                    </Image>
+                </div>
+            </div>
+
+            <div className='about_me'>
+                <div className="about_me_textbox">
+                    <h1 className='about_me_title'>Julia Andrade Dias</h1>
+                    <p className='about_me_subtitle'>Programadora Java</p>
+                    <p className='about_me_rm'>RM: 552332 / 1TDSS</p>
+                    <FaGithub className="icon_about_me"/>
+                    <FaInstagram className="icon_about_me"/>
+                    <FaLinkedinIn className="icon_about_me"/>
+                </div>
+                <div className="about_me_image">
+                    <Image src={'/Contato/ju.svg'} width={200} height={200}>
+                    </Image>
+                </div>
+            </div>
+
+            <div className='about_me'>
+                <div className="about_me_textbox">
+                    <h1 className='about_me_title'>Caio Vitor Urbano Neves</h1>
+                    <p className='about_me_subtitle'>Programadora Python</p>
+                    <p className='about_me_rm'>RM: 552392 / 1TDSS</p>
+                    <FaGithub className="icon_about_me"/>
+                    <FaInstagram className="icon_about_me"/>
+                    <FaLinkedinIn className="icon_about_me"/>
+                </div>
+                <div className="about_me_image">
+                    <Image src={'/Contato/caio.svg'} width={200} height={200}>
+                    </Image>
+                </div>
+            </div>
+
+            <div className='about_me'>
+                <div className="about_me_textbox">
+                    <h1 className='about_me_title'>Guilherme P. de Oliveira</h1>
+                    <p className='about_me_subtitle'>ChatBot e gestão de projetos</p>
+                    <p className='about_me_rm'>RM: 552238 / 1TDSS</p>
+                    <FaGithub className="icon_about_me"/>
+                    <FaInstagram className="icon_about_me"/>
+                    <FaLinkedinIn className="icon_about_me"/>
+                </div>
+                <div className="about_me_image">
+                    <Image src={'/Contato/gui.svg'} width={200} height={200}>
+                    </Image>
+                </div>
             </div>
         </div>
     )
